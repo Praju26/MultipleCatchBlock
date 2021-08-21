bd4bb7c76bf27be6f0aed476c557316c06f0d967
PRAJAKTA <PRAJAKTA@LAPTOP-DQH2B71H>
2021-08-21 14:41:29 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MultipleCatchBlock
+{
+    class Program
+    {
+            static void Main()
+            {
+                int a, b, c;
+                Console.WriteLine("ENTER ANY TWO NUBERS");
+                try
+                {
+                    a = int.Parse(Console.ReadLine());
+                    b = int.Parse(Console.ReadLine());
+                    c = a / b;
+                    Console.WriteLine("C VALUE = " + c);
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("second number should not be zero");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("enter only integer numbers");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+
+
